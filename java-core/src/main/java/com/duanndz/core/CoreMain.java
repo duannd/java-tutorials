04f63d1b89d4f3322116c65214878c3d2a2602c3
Duan Nguyen Dinh <ngdduan@gmail.com>
2020-04-15 22:26:46 +0700

    Add Set example

--- java-core/src/main/java/com/duanndz/core/CoreMain.java
+++ java-core/src/main/java/com/duanndz/core/CoreMain.java
@@ -1,9 +1,21 @@
 package com.duanndz.core;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class CoreMain {
 
     public static void main(String[] args) {
-
+        String ids = "S0152263983260,S0152263983260,S0152263983260,S0152263983260,S0152263983260,2";
+        Set<String> subNumSet = Arrays.stream(ids.split(","))
+            .filter(s -> s != null && !s.trim().isEmpty())
+            .collect(Collectors.toSet());
+        if (subNumSet.size() != 1) {
+            System.out.println("Invalid, many id");
+        } else {
+            System.out.println(subNumSet.iterator().next());
+        }
     }
 
 }
