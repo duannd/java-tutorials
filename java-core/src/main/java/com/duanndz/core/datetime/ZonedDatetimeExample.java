48e87cb1830e90ec5480edd09a9a2b101f4a59ab
Duan Nguyen Dinh <ngdduan@gmail.com>
2020-04-21 09:48:18 +0700

    [Java Core] Add zoned date time to Instant examples

---
+++ java-core/src/main/java/com/duanndz/core/datetime/ZonedDatetimeExample.java
@@ -0,0 +1,28 @@
+package com.duanndz.core.datetime;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+/**
+ * Created By duan.nguyen at 4/21/20 9:43 AM
+ */
+public class ZonedDatetimeExample {
+    public static void main(String[] args) {
+        ZonedDateTime currentPST = ZonedDateTime.now(ZoneOffset.of("-08:00"));
+        System.out.println(currentPST);
+        Instant currentPST2UTC = currentPST.toInstant();
+        System.out.println(currentPST2UTC);
+        Instant currentUTC = Instant.now();
+        System.out.println(currentUTC);
+        ZonedDateTime timeToPushNotification;
+        if (currentPST.getHour() > 8) { // if currentPST > 8 am.
+            timeToPushNotification = currentPST.plusDays(1).withHour(8).withMinute(0).withSecond(0).withNano(0);
+        } else {
+            timeToPushNotification = currentPST.withHour(8).withMinute(0).withSecond(0).withNano(0);
+        }
+        System.out.println(timeToPushNotification);
+        Instant utcTime = timeToPushNotification.toInstant();
+        System.out.println(utcTime);
+    }
+}
