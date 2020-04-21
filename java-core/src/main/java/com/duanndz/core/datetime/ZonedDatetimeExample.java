bbe0cb2946d78cb94202abf6bc3a2b963a4a733e
Duan Nguyen Dinh <ngdduan@gmail.com>
2020-04-21 09:53:06 +0700

    [Java Core] Convert instant to date.

--- java-core/src/main/java/com/duanndz/core/datetime/ZonedDatetimeExample.java
+++ java-core/src/main/java/com/duanndz/core/datetime/ZonedDatetimeExample.java
@@ -3,12 +3,14 @@ package com.duanndz.core.datetime;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 /**
  * Created By duan.nguyen at 4/21/20 9:43 AM
  */
 public class ZonedDatetimeExample {
     public static void main(String[] args) {
+        System.setProperty("user.timezone", "UTC");
         ZonedDateTime currentPST = ZonedDateTime.now(ZoneOffset.of("-08:00"));
         System.out.println(currentPST);
         Instant currentPST2UTC = currentPST.toInstant();
@@ -24,5 +26,7 @@ public class ZonedDatetimeExample {
         System.out.println(timeToPushNotification);
         Instant utcTime = timeToPushNotification.toInstant();
         System.out.println(utcTime);
+        Date dateToPushNotification = new Date(utcTime.toEpochMilli());
+        System.out.println(dateToPushNotification);
     }
 }
