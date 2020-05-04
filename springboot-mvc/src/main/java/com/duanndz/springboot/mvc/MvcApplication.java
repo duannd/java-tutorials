aec7f1840ddce939ad264b350bbbeba0a087349c
duan.nguyen <duan.nguyen@paysmart.com.vn>
2020-05-04 18:00:17 +0700

    [Spring Boot] Add MVC module

---
+++ springboot-mvc/src/main/java/com/duanndz/springboot/mvc/MvcApplication.java
@@ -0,0 +1,17 @@
+package com.duanndz.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * duan.nguyen
+ * Datetime 5/4/20 17:39
+ */
+@SpringBootApplication
+public class MvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcApplication.class, args);
+    }
+
+}
