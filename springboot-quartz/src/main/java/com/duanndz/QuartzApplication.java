448a195262fd247bb91c9f82b9bbfff6cd1a51a0
duan.nguyen <duan.nguyen@paysmart.com.vn>
2020-05-12 11:39:35 +0700

    [Springboot-Quartz] Add quartz using JDBC example

---
+++ springboot-quartz/src/main/java/com/duanndz/QuartzApplication.java
@@ -0,0 +1,17 @@
+package com.duanndz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * duan.nguyen
+ * Datetime 5/11/20 18:03
+ */
+@SpringBootApplication
+public class QuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzApplication.class, args);
+    }
+
+}
