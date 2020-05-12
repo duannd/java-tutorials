dd5c4ba36a7f59b92ee0671d11064f9fcf3aaeb4
duan.nguyen <duan.nguyen@paysmart.com.vn>
2020-05-12 18:32:32 +0700

    [spring-quartz] clean code

--- springboot-quartz/src/main/java/com/duanndz/quartz/QuartzConfiguration.java
+++ springboot-quartz/src/main/java/com/duanndz/quartz/QuartzConfiguration.java
@@ -1,13 +1,11 @@
 package com.duanndz.quartz;
 
-import com.duanndz.quartz.job.WakeupJob;
-import org.quartz.*;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 /**
  * duan.nguyen
@@ -16,46 +14,6 @@ import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 @Configuration
 @EnableScheduling
 public class QuartzConfiguration {
-
-//    private ApplicationContext applicationContext;
-//
-//    @Autowired
-//    public void setApplicationContext(ApplicationContext applicationContext) {
-//        this.applicationContext = applicationContext;
-//    }
-
-//    @Bean
-//    public JobDetailFactoryBean jobDetail() {
-//        JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
-//        jobDetailFactory.setJobClass(WakeupJob.class);
-//        jobDetailFactory.setDescription("Invoke Sample Job service...");
-//        jobDetailFactory.setDurability(true);
-//        return jobDetailFactory;
-//    }
-//
-//    @Bean
-//    public SimpleTriggerFactoryBean trigger(JobDetail jobDetail) {
-//        SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
-//        trigger.setJobDetail(jobDetail);
-//        trigger.setRepeatInterval(30 * 1000); // repeat each 30s
-//        trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
-//        return trigger;
-//    }
-
-//    @Bean
-//    public SpringBeanJobFactory springBeanJobFactory() {
-//        AutoWiringSpringBeanJobFactory jobFactory = new AutoWiringSpringBeanJobFactory();
-//        jobFactory.setApplicationContext(applicationContext);
-//        return jobFactory;
-//    }
-
-//    @Bean
-//    public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
-//        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-//        factory.setJobFactory(springBeanJobFactory());
-////        factory.setQuartzProperties(quartzProperties());
-//        return factory;
-//    }
 
     @Bean
     public Scheduler scheduler(SchedulerFactoryBean quartzScheduler) throws SchedulerException {
